fa17038c4a7e67ac9bdb8def8705a6e6019908f2
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-05-25 10:50:59 +0200

    [MIG] account_cutoff_start_end_dates: added pre_init_hook to handle migration

--- account_invoice_start_end_dates/__init__.py
+++ account_invoice_start_end_dates/__init__.py
@@ -1,1 +1,38 @@
 from . import models
+
+from odoo import api, SUPERUSER_ID
+from openupgradelib import openupgrade
+
+
+def module_migration(cr):
+    account_cutoff_line_model = "account.cutoff.line"
+    account_cutoff_line_table = "account_cutoff_line"
+    account_cutoff_prepaid = "account_cutoff_prepaid"
+    account_cutoff_accrual_dates = "account_cutoff_accrual_dates"
+    account_cutoff_start_end_dates = "account_cutoff_start_end_dates"
+    if openupgrade.is_module_installed(cr, account_cutoff_accrual_dates):
+        openupgrade.update_module_names(
+            cr,
+            [
+                (account_cutoff_accrual_dates, account_cutoff_start_end_dates),
+            ],
+            merge_modules=True,
+        )
+    if openupgrade.is_module_installed(cr, account_cutoff_prepaid):
+        openupgrade.update_module_names(
+            cr,
+            [
+                (account_cutoff_prepaid, account_cutoff_start_end_dates),
+            ],
+            merge_modules=True,
+        )
+        field_renames = [
+            (
+                account_cutoff_line_model,
+                account_cutoff_line_table,
+                "prepaid_days",
+                "cutoff_days",
+            ),
+        ]
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        openupgrade.rename_fields(env, field_renames)
